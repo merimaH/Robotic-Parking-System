e9b5998dccc09aa7bee34dda6c318109a1b47c29
merimaH <mhadziabdi1@etf.unsa.ba>
2018-12-03 19:16:22 +0100

    Added Client and Payment classes

---
+++ RoboticParkingSystem/RoboticParkingSystem/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace RoboticParkingSystem
+{
+    class Client
+    {
+        private String firstName;
+        private String lasteName;
+        private String adress;
+        private Image fingerprint;
+        private Payment payment;
+        private int pinCode;
+
+        public int PinCode
+        {
+            get { return pinCode; }
+            set { pinCode = value; }
+        }
+
+        public Payment Payment
+        {
+            get { return payment; }
+            set { payment = value; }
+        }
+
+        public String Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+        public Image Fingerprint
+        {
+            get { return fingerprint; }
+            set { fingerprint = value; }
+        }
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public String LastName
+        {
+            get { return lasteName; }
+            set { lasteName = value; }
+        }
+        public Client(String firstName,String lastName,String adress,Image fingerprint,Payment payment)
+        {
+            this.firstName = firstName;
+            this.lasteName = lastName;
+            this.adress = adress;
+            this.fingerprint = fingerprint;
+            this.payment = payment;
+        }
+
+
+
+
+    }
+}
---
+++ RoboticParkingSystem/RoboticParkingSystem/Payment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboticParkingSystem
+{
+    class Payment
+    {
+        private DateTime dateOfPayment;
+        private int numberPaidMonths;
+
+        public DateTime DateOfPayment
+        {
+            get { return dateOfPayment; }
+            set { dateOfPayment = value; }
+        }
+      
+        public int NumberPaidMonths
+        {
+            get { return numberPaidMonths; }
+            set { numberPaidMonths = value; }
+        }
+        
+    }
+}
