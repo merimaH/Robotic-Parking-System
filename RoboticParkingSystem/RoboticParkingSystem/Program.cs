3b5b90b905d849cd56e249620a39cf1d749c98fe
Pašalić Sedin <spasalic1@etf.unsa.ba>
2019-01-22 22:23:45 +0100

    Dodano parkiranje

--- RoboticParkingSystem/RoboticParkingSystem/Program.cs
+++ RoboticParkingSystem/RoboticParkingSystem/Program.cs
@@ -16,7 +16,7 @@ namespace RoboticParkingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new pocetna());
         }
     }
 }
